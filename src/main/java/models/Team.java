ce533f84ee00d7512927e5a6c75eaee37332115e
Guest User <Guest@Epicodus-5E.local>
2017-08-11 08:59:15 -0700

    test fails

--- src/main/java/models/Team.java
+++ src/main/java/models/Team.java
@@ -1,7 +1,31 @@
 package models;
 
+import java.util.ArrayList;
+
+import static spark.Spark.staticFileLocation;
+
 /**
  * Created by Guest on 8/11/17.
  */
 public class Team {
+        private String name;
+        private String description;
+        private static ArrayList<Team> instances = new ArrayList<>();
+
+        public Team(String name, String description)    {
+            this.name=name;
+            this.description=description;
+        }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public static ArrayList<Team> getInstances() {
+        return instances;
+    }
 }
---
+++ src/test/java/models/TeamTest.java
@@ -0,0 +1,31 @@
+package models;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Guest on 8/11/17.
+ */
+public class TeamTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void NewObjectsGetCorrectlyCreated_true() throws Exception   {
+        Team team = new Team("Codeagedon","Team of coders from Epicodus");
+    assertEquals(true,team instanceof Team);
+    }
+    @Test
+    public void PostInstantiatesWithNewContent_true()   throws Exception    {
+        Team team = new Team("Codeagedon","Team of coders from Epicodus");
+        assertEquals("Codeagedon: Team of coders from Epicodus", team.getName());
+    }
+}
