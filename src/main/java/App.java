735a4c5fe0703f4dd5f3062eb7abd665f9a762d7
Collin Person <collin.person@gmail.com>
2017-08-11 10:08:03 -0700

    launch homepage :D

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,19 @@
-/**
- * Created by Guest on 8/11/17.
- */
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import static spark.Spark.*;
+import models.Team;
+
 public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+
+        get("/",(request, response) -> {
+            Map<String,Object> model = new HashMap<>();
+            return new ModelAndView(model, "index.hbs");
+        },   new HandlebarsTemplateEngine());
+
+    }
 }
