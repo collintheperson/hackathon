f79d12d06bdce24ad225e43ec466ef34c9dc8612
Collin Person <collin.person@gmail.com>
2017-08-11 10:53:46 -0700

    add better names for variables and paths

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -15,10 +15,29 @@ public class App {
             return new ModelAndView(model, "index.hbs");
         },   new HandlebarsTemplateEngine());
 
-        get("/posts/new",(request, response) -> {
+        get("/teams/new",(request, response) -> {
             Map<String,Object> model = new HashMap<>();
-            return new ModelAndView(model, "post-form.hbs");
+            return new ModelAndView(model, "team-form.hbs");
         },   new HandlebarsTemplateEngine());
 
+        post("/teams/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+
+            String content = request.queryParams("name");
+            String content2 = request.queryParams("description");
+            System.out.println(content);
+            System.out.println(content2);
+            Team newTeam = new Team(content,content2);
+            model.put("team", newTeam);
+            return new ModelAndView(model, "success.hbs");
+        }, new HandlebarsTemplateEngine());
+        //get: show all posts
+
+        get("/teams", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            ArrayList<Team> teams = Team.getAll();
+            model.put("teams", teams);
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
